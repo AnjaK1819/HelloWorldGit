ae42f7d4a6b02d45db58230edb05eef4af24fe82
AnjaK1819 <anjakriechhammer@Anjas-MacBook-Pro.local>
2018-12-19 10:25:31 +0100

    init project

---
+++ Assets/MyGame/Script/Basics.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Basics : MonoBehaviour {
+
+    public Text overrideText;
+    
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Hallo");
+        overrideText.text = "Lirum-Ipsum lala";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	//	Debug.Log("alive");
+	}
+}
+
